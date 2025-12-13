03df318df82b4f0f5e6dfb3b9424ce3a40c956b9
bala-amypo <bala@amypo.in>
2025-12-13 09:48:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/models/Guest.java
@@ -0,0 +1,84 @@
+package com.example.demo.model;
+public class Guest {
+
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private Boolean verified;
+
+    private Boolean active;
+
+    private String role;
+
+    private Timestamp createdAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/example/demo/models/RoomBooking.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class RoomBooking{
+    private long id;
+    private String fullName;
+    private String email;
+    private Sttring phoneNumber;
+    private Boolean verified;
+    private Boolean active;
+    private String role;
+    
+}
