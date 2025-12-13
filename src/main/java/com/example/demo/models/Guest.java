0bdc74d0863ada5c3024aebf5c746062010c1f80
bala-amypo <bala@amypo.in>
2025-12-13 09:53:25 +0000

    User commit

--- src/main/java/com/example/demo/models/Guest.java
+++ src/main/java/com/example/demo/models/Guest.java
@@ -1,23 +1,14 @@
 package com.example.demo.model;
-public class Guest {
 
+public class RoomBooking{
     private Long id;
-
     private String fullName;
-
     private String email;
-
     private String phoneNumber;
-
     private Boolean verified;
-
     private Boolean active;
-
     private String role;
-
     private Timestamp createdAt;
-
-
     public Long getId() {
         return id;
     }
@@ -25,60 +16,12 @@ public class Guest {
     public void setId(Long id) {
         this.id = id;
     }
-
-    public String getFullName() {
+    public String getfullName(){
         return fullName;
     }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setfullName(String fullName){
+        this.fullName = f
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Boolean getVerified() {
-        return verified;
-    }
-
-    public void setVerified(Boolean verified) {
-        this.verified = verified;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
 }
