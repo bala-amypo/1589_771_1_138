ec1985313d8cf9672e9886d99d83a59bbc26d544
bala-amypo <bala@amypo.in>
2025-12-16 08:35:41 +0000

    User commit

--- src/main/java/com/example/demo/models/Guest.java
+++ src/main/java/com/example/demo/models/Guest.java
@@ -2,4 +2,11 @@ package com.example.demo.models;
 
 public class Guest {
     private long id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private Boolean verified;
+    private String role;
+    private Timestamp createdAt;
+    public
 }
