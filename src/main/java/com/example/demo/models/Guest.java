e50a2fbb34d2c85d20d3f04a2694edc07410bb76
bala-amypo <bala@amypo.in>
2025-12-16 08:40:41 +0000

    User commit

--- src/main/java/com/example/demo/models/Guest.java
+++ src/main/java/com/example/demo/models/Guest.java
@@ -1,12 +1,13 @@
 package com.example.demo.models;
-
 public class Guest {
     private long id;
     private String fullName;
     private String email;
     private String phoneNumber;
     private Boolean verified;
+    private Boolean active;
     private String role;
     private Timestamp createdAt;
-    public
+    public Guest(){}
+    public Guest(Long id,String fullName,String email,String phoneNumber,Boolean verified,Boolean active,Stri)
 }
