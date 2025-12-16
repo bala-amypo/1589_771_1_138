722a71169b49aecd6cbc74f20e12faa66c04fc79
bala-amypo <bala@amypo.in>
2025-12-16 15:45:53 +0000

    User commit

--- src/main/java/com/example/demo/models/AccessLog.java
+++ src/main/java/com/example/demo/models/AccessLog.java
@@ -1,16 +1,21 @@
 package com.example.demo.model;
-import jakarta.persisten
+import jakarta.persistence.*;
 import java.security.Timestamp;
 @Entity
 public class AccessLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @ManyToOne
+    @JoinColumn(name = "digital_key_id" , nullable = false)
     private DigitalKey digitalKey;
+    @ManyToOne
+    @JoinColumn(name = "guest_id" , nullable = false)
     private Guest guest;
     private Timestamp accessTime;
     private String result;
     private String reason;
+    public Accesslog(){}
     public AccessLog(DigitalKey digitalKey, Guest guest, Timestamp accessTime, String result, String reason) {
         this.digitalKey = digitalKey;
         this.guest = guest;
