47f52b53be3ecdf431ccf5dc68ffbf4107071aa5
bala-amypo <bala@amypo.in>
2025-12-17 10:04:34 +0000

    User commit

--- src/main/java/com/example/demo/model/DigitalKey.java
+++ src/main/java/com/example/demo/model/DigitalKey.java
@@ -1,67 +1,67 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import java.time.LocalDateTime;
-// import jakarta.persistence.*;
-// @Entity
-// public class DigitalKey {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private RoomBooking booking;
-//     @Column(unique = true)
-//     private String keyValue;
-//     private LocalDateTime issuedAt;
-//     private LocalDateTime expiresAt;
-//     private Boolean active = true;
-//     public DigitalKey(RoomBooking booking, String keyValue, Timestamp issuedAt, Timestamp expiresAt, Boolean active) {
-//         this.booking = booking;
-//         this.keyValue = keyValue;
-//         this.issuedAt = issuedAt;
-//         this.expiresAt = expiresAt;
-//         this.active = active;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public RoomBooking getBooking() {
-//         return booking;
-//     }
-//     public String getKeyValue() {
-//         return keyValue;
-//     }
-//     public Timestamp getIssuedAt() {
-//         return issuedAt;
-//     }
-//     public Timestamp getExpiresAt() {
-//         return expiresAt;
-//     }
-//     public Boolean getActive() {
-//         return active;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public void setBooking(RoomBooking booking) {
-//         this.booking = booking;
-//     }
-//     public void setKeyValue(String keyValue) {
-//         this.keyValue = keyValue;
-//     }
-//     public void setIssuedAt(Timestamp issuedAt) {
-//         this.issuedAt = issuedAt;
-//     }
-//     public void setExpiresAt(Timestamp expiresAt) {
-//         this.expiresAt = expiresAt;
-//     }
-//     public void setActive(Boolean active) {
-//         this.active = active;
-//     }
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+@Entity
+public class DigitalKey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private RoomBooking booking;
+    @Column(unique = true)
+    private String keyValue;
+    private LocalDateTime issuedAt;
+    private LocalDateTime expiresAt;
+    private Boolean active = true;
+    public DigitalKey(RoomBooking booking, String keyValue, Timestamp issuedAt, Timestamp expiresAt, Boolean active) {
+        this.booking = booking;
+        this.keyValue = keyValue;
+        this.issuedAt = issuedAt;
+        this.expiresAt = expiresAt;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public RoomBooking getBooking() {
+        return booking;
+    }
+    public String getKeyValue() {
+        return keyValue;
+    }
+    public Timestamp getIssuedAt() {
+        return issuedAt;
+    }
+    public Timestamp getExpiresAt() {
+        return expiresAt;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setBooking(RoomBooking booking) {
+        this.booking = booking;
+    }
+    public void setKeyValue(String keyValue) {
+        this.keyValue = keyValue;
+    }
+    public void setIssuedAt(Timestamp issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+    public void setExpiresAt(Timestamp expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
      
-//     @PrePersist
-//     @PreUpdate
-//     private void validateDates(){
-//         if(expiresAt.isBefore(issuedAt)){
-//             throw new IllegalStateException("Expiration data must be after or equal to the issued date.");
-//         }
-//     }
-// }
+    @PrePersist
+    @PreUpdate
+    private void validateDates(){
+        if(expiresAt.isBefore(issuedAt)){
+            throw new IllegalStateException("Expiration data must be after or equal to the issued date.");
+        }
+    }
+}
