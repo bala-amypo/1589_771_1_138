9f17b87648d52f942eba2931f134b8c7ded0dc0d
bala-amypo <bala@amypo.in>
2025-12-19 16:15:24 +0000

    User commit

--- src/main/java/com/example/demo/service/GuestService.java
+++ src/main/java/com/example/demo/service/GuestService.java
@@ -2,8 +2,16 @@ package com.example.demo.service;
 
 import com.example.demo.model.Guest;
 import java.util.List;
+import java.util.Optional;
+
 public interface GuestService {
+    // For AuthController /register
     Guest createGuest(Guest guest); 
+
+    // For AuthController /login
+    Guest loginGuest(String email, String password);
+
+    // Standard CRUD
     Guest updateGuest(Long id, Guest guest); 
     Guest getGuestById(Long id);
     List<Guest> getAllGuests();
