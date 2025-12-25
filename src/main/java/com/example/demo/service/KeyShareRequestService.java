d76a3369a480da6a4364ff957f40e237fc566188
bala-amypo <bala@amypo.in>
2025-12-25 05:05:10 +0000

    User commit

--- src/main/java/com/example/demo/service/KeyShareRequestService.java
+++ src/main/java/com/example/demo/service/KeyShareRequestService.java
@@ -4,9 +4,12 @@ import com.example.demo.model.KeyShareRequest;
 import java.util.List;
 
 public interface KeyShareRequestService {
+
     KeyShareRequest createShareRequest(KeyShareRequest request);
-    KeyShareRequest updateStatus(Long id, String status);
-    KeyShareRequest getRequestById(Long id);
+
     List<KeyShareRequest> getSharedBy(Long guestId);
+
     List<KeyShareRequest> getSharedWith(Long guestId);
+
+    KeyShareRequest getRequestById(Long id);
 }
